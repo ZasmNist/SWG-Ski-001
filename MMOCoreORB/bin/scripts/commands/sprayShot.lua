2d5f9c53c4231fe8885be67b9cf62bb666a27edc
matt <27567248+ZasmNist@users.noreply.github.com>
2018-03-02 19:43:48 -0700

    Update sprayShot.lua

--- MMOCoreORB/bin/scripts/commands/sprayShot.lua
+++ MMOCoreORB/bin/scripts/commands/sprayShot.lua
@@ -44,7 +44,7 @@
 SprayShotCommand = {
         name = "sprayshot",
 
-	damageMultiplier = 4.0,
+	damageMultiplier = 5.0,
 	speedMultiplier = 3.5,
 	healthCostMultiplier = 1,
 	actionCostMultiplier = 1,
@@ -83,9 +83,9 @@ SprayShotCommand = {
 		{}, 
 		{}, 
 		{ "force_defense" }, 
-		100, 
+		75, 
 		0, 
-		90 
+		60 
 	  )
 	},
 
