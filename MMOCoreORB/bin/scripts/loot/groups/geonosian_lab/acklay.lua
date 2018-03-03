f138578e89cd8adcba8f74ab9250cd3229996c7c
matt <27567248+ZasmNist@users.noreply.github.com>
2018-03-02 22:09:20 -0700

    Update acklay.lua

--- MMOCoreORB/bin/scripts/loot/groups/geonosian_lab/acklay.lua
+++ MMOCoreORB/bin/scripts/loot/groups/geonosian_lab/acklay.lua
@@ -5,12 +5,12 @@ acklay = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "acklay_bone_armor_schematic", weight = 1600000},
-		{itemTemplate = "acklay_bones", weight = 2200000},
-		{itemTemplate = "acklay_bones_rare", weight = 200000},
-		{itemTemplate = "acklay_hide", weight = 2200000},
-		{itemTemplate = "acklay_ris_armor_schematic", weight = 1600000},
-		{itemTemplate = "acklay_venom", weight = 2200000}
+		{itemTemplate = "acklay_bone_armor_schematic", weight = 500000},
+		{itemTemplate = "acklay_bones", weight = 4000000},
+		{itemTemplate = "acklay_bones_rare", weight = 2300000},
+		{itemTemplate = "acklay_hide", weight = 500000},
+		{itemTemplate = "acklay_ris_armor_schematic", weight = 2200000},
+		{itemTemplate = "acklay_venom", weight = 500000}
 	}
 }
 
