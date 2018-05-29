15298eed839eccd684e57a3e6ba9ff7eb1dec206
matt <27567248+ZasmNist@users.noreply.github.com>
2018-05-28 22:34:33 -0600

    DWB MOB Update - Overlord
    resists updated. added lightsaber resists. additional low % chance loots. damage incresed. specials added.

--- MMOCoreORB/bin/scripts/mobile/dungeon/death_watch_bunker/death_watch_overlord.lua
+++ MMOCoreORB/bin/scripts/mobile/dungeon/death_watch_bunker/death_watch_overlord.lua
@@ -4,15 +4,15 @@ death_watch_overlord = Creature:new {
 	socialGroup = "death_watch",
 	pvpFaction = "death_watch",
 	faction = "",
-	level = 350,
+	level = 330,
 	chanceHit = 190,
-	damageMin = 1245,
-	damageMax = 2200,
-	baseXp = 20948,
+	damageMin = 1700,
+	damageMax = 2700,
+	baseXp = 35000,
 	baseHAM = 400000,
 	baseHAMmax = 400000,
 	armor = 3,
-	resists = {70,70,70,70,45,45,70,70,5},
+	resists = {60,70,70,65,35,25,60,10,5},--kinetic,energy,blast,heat,cold,electric,acid,stun,ls
 	meatType = "",
 	meatAmount = 0,
 	hideType = "",
@@ -24,8 +24,9 @@ death_watch_overlord = Creature:new {
 	ferocity = 0,
 	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
 	creatureBitmask = KILLER,
-	optionsBitmask = 128,
+	optionsBitmask = AIENABLED,
 	diet = HERBIVORE,
+	scale = 2.0,
 
 	templates = {"object/mobile/dressed_death_watch_gold.iff"},
 	lootGroups = {
@@ -33,7 +34,7 @@ death_watch_overlord = Creature:new {
 			groups = {
 				{group = "death_watch_bunker_overlord_shared", chance =  10000000}
 			},
-			lootChance = 10000000
+			lootChance = 5000000
 		},
 		{
 			groups = {
@@ -47,32 +48,32 @@ death_watch_overlord = Creature:new {
 				{group = "clothing_attachments", chance = 5000000}
 				{group = "clothing_attachments", chance = 5000000},
 			},
-			lootChance = 1000000
+			lootChance = 2000000
 		},
 		{
 			groups = {
 				{group = "clothing_attachments", chance = 5000000}
-				{group = "clothing_attachments", chance = 5000000},
+				{group = "armor_attachments", chance = 5000000},
 			},
-			lootChance = 1000000
+			lootChance = 2000000
 		},
 		{
 			groups = {
 				{group = "axkva_min", chance = 10000000},
 			},
-			lootChance = 5000000
+			lootChance = 2000000
 		},
 		{
 	       		groups = {
 				{group = "krayt_tissue_rare", chance = 4000000},
 				{group = "pearls_flawless", chance = 6000000}				
 			},
-			lootChance = 9000000
+			lootChance = 2000000
 		}
 		},
 	weapons = {"dark_trooper_weapons"},
 	conversationTemplate = "",
-	attacks = merge(riflemanmaster,pistoleermaster,carbineermaster,brawlermaster)
+	attacks = merge(riflemanmaster,pistoleermaster,carbineermaster,brawlermaster,fencermaster,marksmanmaster)
 }
 
 CreatureTemplates:addCreatureTemplate(death_watch_overlord, "death_watch_overlord")
