b914df73930cb84923a8b8b6785dbc08156cf494
matt <27567248+ZasmNist@users.noreply.github.com>
2018-05-31 19:51:09 -0600

    Added Dark Jedi Mission
    spawns x3

---
+++ MMOCoreORB/bin/scripts/mobile/lair/creature_mission/dark_jedi_1.lua
@@ -0,0 +1,11 @@
+dark_jedi_1 = Lair:new {
+	mobiles = {},
+	spawnLimit = 3,
+	buildingsVeryEasy = {},
+	buildingsEasy = {},
+	buildingsMedium = {},
+	buildingsHard = {},
+	buildingsVeryHard = {},
+}
+
+addLairTemplate("dark_jedi_1", dark_jedi_1)
